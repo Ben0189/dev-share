d224b10726afb8bf2ff645141319112c5779fae5
Xu-create-ops <cxu498148@gmail.com>
2025-07-12 16:41:29 +0800

    Update Unit test

--- dev-share-api.Tests/Services/UrlManageUtilTest.cs
+++ dev-share-api.Tests/Services/UrlManageUtilTest.cs
@@ -2,23 +2,18 @@ using Xunit;
 using Microsoft.Extensions.DependencyInjection;
 using Services;
 
-public class TestCommon
+public class UrlManageUtilTest
 {
     
     private readonly IResourceService _resourceService;
-
-    public TestCommon()
-    {
-        var provider = TestHost.BuildTestServiceProvider();
-        _resourceService = provider.GetRequiredService<IResourceService>();
-    }
+    
     
     [Fact]
-    public async void GetResource()
+    public async void NormalizeUrl()
     {
         var normalizeUrl = UrlManageUtil.NormalizeUrl(
             "https://www.jetbrains.com/academy/career-in-it/?source=google&medium=cpc&campaign=APAC_en_ASIA_JBAcademy_Boost_Your_Career_Search&term=learn%20java&content=726159867494&gad_source=1&gad_campaignid=22045194704&gbraid=0AAAAADloJzgkigv1D38jQ0Jl58NdqcZLe&gclid=CjwKCAjw4K3DBhBqEiwAYtG_9HNYVMG-wpMeOEeWGKnFU4ocVZn_QwP1uoBOUseJAaIPKDyq9i_nOBoCEUIQAvD_BwE");
         
-        Console.WriteLine(normalizeUrl);
+        Assert.Equal(normalizeUrl, "https://www.jetbrains.com/academy/career-in-it?campaign=APAC_en_ASIA_JBAcademy_Boost_Your_Career_Search&content=726159867494&gad_campaignid=22045194704&gad_source=1&gbraid=0AAAAADloJzgkigv1D38jQ0Jl58NdqcZLe&gclid=CjwKCAjw4K3DBhBqEiwAYtG_9HNYVMG-wpMeOEeWGKnFU4ocVZn_QwP1uoBOUseJAaIPKDyq9i_nOBoCEUIQAvD_BwE&medium=cpc&source=google&term=learn+java");
     }
 }
